77265f1c776bc7d34272cfd8957a98705c65618d
Alex <o.a.momotyuk@gmail.com>
2019-08-28 10:39:19 -0500

    version 2 initial files

---
+++ src/journal.js
@@ -0,0 +1,25 @@
+/*
+//
+*/
+/*
+You defined form fields for the following bits of data to collect from a user of your application.
+
+Date of the journal entry.
+Concepts covered (which will be the title of the entry).
+The long-form contents of the journal entry.
+The mood of the journal entry.
+In your JavaScript file, define an object that will represent a journal entry in your code.
+*/
+/*
+    Define the keys and value for a JavaScript object that represents a journal entry about what you learned today
+*/
+const journalEntries = [];
+
+const journalEntry = {
+    entryDate: "2019.08.28",
+    entryTitle: "Objects Representing Things",
+    entryContents: "Since objects are the core concept behind Object Oriented Programming - what you are learning here at NSS - you are going to learn how to use objects to provide value and context to data in your applications. As developers, we write software in order to automate processes that people already perform with real world things. Your code will represent those people and things as objects.",
+    entryMood: "good"
+}
+
+
