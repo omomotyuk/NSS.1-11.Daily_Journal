da7a180ccb25f9db49f316c21e537c3c736f0da3
Alex <o.a.momotyuk@gmail.com>
2019-09-03 13:26:09 -0500

    daily journal part 4 - done

--- src/journal.js
+++ src/journal.js
@@ -1,55 +1,6 @@
 /*
 //
 */
-/*
-    You defined form fields for the following bits of data to collect from a user of your application.
-
-    Date of the journal entry.
-    Concepts covered (which will be the title of the entry).
-    The long-form contents of the journal entry.
-    The mood of the journal entry.
-    In your JavaScript file, define an object that will represent a journal entry in your code.
-*/
-/*
-    Define the keys and value for a JavaScript object that represents a journal entry about what you learned today
-*/
-const journalEntries = [];
-
-//
-function journalEntry ( date, title, content, mood ){ 
-    this.date = date;
-    this.title = title;
-    this.content = content;
-    this.mood = mood;
-}
-
-//
-const entry01 = new journalEntry (
-    "2019.08.27",
-    "Array methods",
-    "We learned about array methods, but only forEach made sense.",
-    "good"
-)
-const entry02 = new journalEntry (
-    "2019.08.28",
-    "Objects Representing Things",
-    "Since objects are the core concept behind Object Oriented Programming - what you are learning here at NSS - you are going to learn how to use objects to provide value and context to data in your applications. As developers, we write software in order to automate processes that people already perform with real world things. Your code will represent those people and things as objects.",
-    "good"
-)
-const entry03 = new journalEntry (
-    "2019.08.29",
-    "Building DOM Components",
-    "Creating an independent component that can be used anywhere in your application has been a goal of JavaScript developers since the mid-2000s. The reason being that component-based systems are infinitely flexible and composable, allowing for faster development and modifications of features.",
-    "good"
-)
-
-//
-journalEntries.push( entry01 );
-journalEntries.push( entry02 );
-journalEntries.push( entry03 );
-
-//console.log( "Journal Entries: ", journalEntries );
-console.table( journalEntries );
 
 /*
     Purpose: To create, and return, a string template that represents a single journal entry object as HTML
@@ -66,21 +17,21 @@ const createJournalEntryComponent = ( journalEntry ) => {
     `
 }
 
-/*
-    Now write a function whose reponsibility is to iterate your array of journal entries and add them to the DOM. 
-    Purpose: To render all journal entries to the DOM
-    Arguments: entries (array of objects)
-*/
 //
 const sectionElement = document.getElementById('entryLog');
 
 //
-const renderJournalEntries = (entries) => {
-    for( let i = 0; i < entries.length; i++ ) {
-        const articleElement = createJournalEntryComponent( entries[i] );
-        sectionElement.innerHTML += articleElement;
-    }
+function getData() {
+    fetch( "http://localhost:3000/entries" ) // Fetch from the API
+    .then( data => data.json() )  // Parse as JSON
+    .then( parsed_data => {
+        //console.table( parsed_data )
+        parsed_data.forEach( (record,index) => {
+            //console.log( index, "record", record)
+            sectionElement.innerHTML += createJournalEntryComponent( record );
+        })
+    })
 }
 
-// Invoke the render function
-renderJournalEntries( journalEntries )
+//
+getData()
