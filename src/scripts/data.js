1bcc6141b84741a176b9b5f2a075e4fa1dbaa4d2
Alex <o.a.momotyuk@gmail.com>
2019-09-13 09:06:27 -0500

    added first entries to json file

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -7,7 +7,21 @@ const API = {
     getJournalEntries () {
         return fetch("http://localhost:3000/entries")
             .then(response => response.json())
+    },
+    
+    editEntries: ( entry ) => {
+        //const donutUpdateObject = {
+        //    name: document.querySelector("#donutName").value
+        //}
+        return fetch(`http://localhost:3000/entries`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify( entry )
+        }).then(response => response.json())
     }
+
 }
 
 export default API
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -3,7 +3,78 @@
 */
 
 import selectElement from "./select.js"
+import createJournalEntryComponent from "./entryComponent.js"
+import API from "./data.js"
 import sectionElement from "./entriesDOM.js"
+
+
+//const journalEntries = [];
+
+//
+/*function journalEntry ( date, title, content, mood ){ 
+    this.date = date;
+    this.title = title;
+    this.content = content;
+    this.mood = mood;
+}*/
+
+let newEntry = ( date, title, content, mood ) => {
+    const entry = {
+        "date": date,
+        "title": title,
+        "content": content,
+        "mood": mood
+    }
+    return entry    
+}
+/*
+"id": "1",
+"date": "2019.08.27",
+"title": "Array methods",
+"content": "We learned about array methods, but only forEach made sense.",
+"mood": "good"
+*/
+
+//
+/*const entry01 = new journalEntry (
+    "2019.08.27",
+    "Array methods",
+    "We learned about array methods, but only forEach made sense.",
+    "good"
+)
+*/
+
+const getValue = ( id ) => {
+    console.log( document.querySelector(`#${id}`).value ) 
+    return document.querySelector(`#${id}`).value 
+}
+
+const getHTML = ( id ) => {
+    return document.querySelector(`#${id}`).innerHTML
+}
+
+//let currentEntry = 
+
+let inputElement = document.querySelector( "#input-form__button" )
+
+inputElement.addEventListener( "click", (event) => {
+
+    let entry = newEntry( getValue('dateOfEntry'), getValue('coveredConcepts'), getHTML('journalEntry'), getValue('mood') )
+
+    console.log( "entry: ", entry )
+
+    console.log( createJournalEntryComponent( entry ) )
+
+    API.editEntries( entry )
+
+    sectionElement.innerHTML += createJournalEntryComponent( entry );
+
+    //return entry
+})
+
+//if( currentEntry !=  ) console.log( "currentEntry: ", currentEntry )
+
+
 
 /*
 <script src="src/select.js"></script>
