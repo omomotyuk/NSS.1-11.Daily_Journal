37b31f6369a482d24172121d543bd2c430268502
Alex <o.a.momotyuk@gmail.com>
2019-09-17 11:01:38 -0500

    short list for mood selector

--- src/scripts/selectItems.js
+++ src/scripts/selectItems.js
@@ -4,7 +4,8 @@
 
 //
 const selectItems = () => {
-    let selectItems = [ "Joy", "Fear", "Anger", "Frustrated", "Acceptance", "Excited", "Confusion", "Curious", "Surprise", "Determined", "Hopeful", "Happy", "Fine", "Sad", "Anxious", "Love", "Sadness", "Wonder", "Rejuvenated", "Drained", "Confident", "Amused", "Guilty", "Relieved", "Indifferent", "Good", "Ok" ];
+    //let selectItems = [ "Joy", "Fear", "Anger", "Frustrated", "Acceptance", "Excited", "Confusion", "Curious", "Surprise", "Determined", "Hopeful", "Happy", "Fine", "Sad", "Anxious", "Love", "Sadness", "Wonder", "Rejuvenated", "Drained", "Confident", "Amused", "Guilty", "Relieved", "Indifferent", "Good", "Ok" ];
+    let selectItems = [ "Sad", "Happy", "Ok" ]
     return selectItems.sort()
 }
 
