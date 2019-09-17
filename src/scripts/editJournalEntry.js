3ec637dc4f0a9430580f0e19783bb57012831f4c
Alex <o.a.momotyuk@gmail.com>
2019-09-17 18:52:07 -0500

    part-13 - first step to data normalization

--- src/scripts/editJournalEntry.js
+++ src/scripts/editJournalEntry.js
@@ -8,7 +8,7 @@ const editJournalEntry = ( entry ) => {
     document.querySelector( "#dateOfEntry" ).value = entry.date
     document.querySelector( "#coveredConcepts" ).value =  entry.title
     document.querySelector( "#journalEntry" ).value =  entry.content
-    document.querySelector( "#mood" ).value =  entry.mood
+    document.querySelector( "#mood" ).value =  entry.moodId
     document.querySelector( "#input-form__button" ).value =  entry.id
 }
 
--- src/scripts/entryComponent.js
+++ src/scripts/entryComponent.js
@@ -9,7 +9,7 @@ const createJournalEntryComponent = ( journalEntry ) => {
         <article class="log-item">
             <h3><span>${journalEntry.date}</span> ${journalEntry.title}</h3>
             <p>${journalEntry.content}</p>
-            <p><span>Mood:</span> ${journalEntry.mood}</p>
+            <p><span>Mood:</span> ${journalEntry.moodId}</p>
             <button id="delete-id-${journalEntry.id}">Delete</button>
             <button id="edit-id-${journalEntry.id}">Edit</button>
         </article>
